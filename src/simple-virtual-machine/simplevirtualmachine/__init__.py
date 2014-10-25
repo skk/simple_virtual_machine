8cc7abe944c1c5f691fba5aec0f45d05c02a1dbf
Steven Knight <steven@knight.cx>
2014-10-25 18:10:42 -0400

    Initial package structure

---
+++ src/simple-virtual-machine/simplevirtualmachine/__init__.py
@@ -0,0 +1,22 @@
+'''
+simple-virtual-machine: Main module
+
+Copyright 2014, Steven Knight
+Licensed under MIT.
+'''
+
+
+def main():
+    '''
+    Main function of the boilerplate code is the entry point of the 'simplevirtualmachine' executable script (defined in setup.py).
+    
+    Use doctests, those are very helpful.
+    
+    >>> main()
+    Hello
+    >>> 2 + 2
+    4
+    '''
+    
+    print("Hello")
+
