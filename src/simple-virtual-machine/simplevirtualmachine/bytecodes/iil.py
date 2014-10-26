8f1fba2149659155ff0a50749c2ac77ad545f082
Steven Knight <steven@knight.cx>
2014-10-26 19:19:03 -0400

    Corrected typo

--- src/simple-virtual-machine/simplevirtualmachine/bytecodes/iil.py
+++
@@ -1,8 +0,0 @@
-import simplevirtualmachine.bytecodes.instruction
-
-class IIL(simplevirtualmachine.bytecodes.instruction.Instruction):
-    """IIL instruction."""
-    def __init__(self):
-        super(IIL, self).__init__(4)
-
-
---
+++ src/simple-virtual-machine/simplevirtualmachine/bytecodes/ilt.py
@@ -0,0 +1,9 @@
+import simplevirtualmachine.bytecodes.instruction
+
+class ILT(simplevirtualmachine.bytecodes.instruction.Instruction):
+    """ILT instruction."""
+    def __init__(self):
+        super(ILT, self).__init__(4)
+        
+        
+        
