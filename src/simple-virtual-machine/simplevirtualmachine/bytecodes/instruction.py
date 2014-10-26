94cd8a34263531377e42fbfec92e4a2c93353a1a
Steven Knight <steven@knight.cx>
2014-10-26 19:20:45 -0400

    Store all opcodes supported by VM

--- src/simple-virtual-machine/simplevirtualmachine/bytecodes/instruction.py
+++ src/simple-virtual-machine/simplevirtualmachine/bytecodes/instruction.py
@@ -1,11 +1,17 @@
 class Instruction(object):
     """ An instruction in our simplevirtualmachine."""
+    opcodes = {}
     
     def __init__(self, opcode, operand_count = 0):
         self.opcode = opcode
         self.operand_count = operand_count
-
+        Instruction.opcodes[self] = self
+        
     def __str__(self):
         return "opcode: {0}, operand_count: {1}".format(
             self.opcode, self.operand_count)
-
+    
+    @classmethod
+    def to_instruction_from_opcode(cls, opcode):
+        return cls.opcodes[opcode]
+    
