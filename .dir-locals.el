29cbe4b0794aab492b50edc906c44c4fc9955d4b
Steven Knight <steven@knight.cx>
2014-10-29 17:38:57 -0400

    emacs dir. variables

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil . (
+         (flycheck-python-pylint-executable . "/home/skk/.virtualenvs/svm-virenv/bin/pylint")
+         (flycheck-python-flake8-executable . "/home/skk/.virtualenvs/svm-virenv/bin/flake8")
+         (python-check-command . "/home/skk/.virtualenvs/svm-virenv/bin/flake8")
+         )))
