12c3a92531273c28c4e43f3652f92cb07dd87f97
Steven Knight <steven@knight.cx>
2014-10-29 18:19:23 -0400

    Set  elpy-rpc-python-comamnd and elpy-set-test-runner

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,6 @@
          (flycheck-python-pylint-executable . "/home/skk/.virtualenvs/svm-virenv/bin/pylint")
          (flycheck-python-flake8-executable . "/home/skk/.virtualenvs/svm-virenv/bin/flake8")
          (python-check-command . "/home/skk/.virtualenvs/svm-virenv/bin/flake8")
+         (elpy-rpc-python-comamnd . "/home/skk/.virtualenvs/svm-virenv/bin/python2")
+         (elpy-set-test-runner . "py.test")
          )))
