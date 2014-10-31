037f23041f0b87db36574aa3d98e400148f38ff2
Steven Knight <steven@knight.cx>
2014-10-30 20:24:40 -0400

    Added name of Bytecode class

--- src/simple-virtual-machine/simplevirtualmachine/bytecodes.py
+++ src/simple-virtual-machine/simplevirtualmachine/bytecodes.py
@@ -10,36 +10,37 @@ class Bytecode(object):
     """ A bytecode in our simple virtual machine."""
     opcodes = {}
             
-    def __init__(self, opcode, operand_count=0):
+    def __init__(self, name, opcode, operand_count=0):
+        self.name = name
         self.opcode = opcode
         self.operand_count = operand_count
-        Bytecode.opcodes[self] = self
+        Bytecode.opcodes[self.opcode] = self
                 
     def __str__(self):
-        return "opcode: {0}, operand_count: {1}".format(
-            self.opcode, self.operand_count)
+        return "Bytecode name: {}\topcode: {:02d}\toperand_count: {:02d}".format(
+            self.name.ljust(10), self.opcode, self.operand_count)
                 
     @classmethod
     def to_instruction_from_opcode(cls, opcode):
         return cls.opcodes[opcode]
 
 
-INVALID = Bytecode(0)
-IADD = Bytecode(1)
-ISUB = Bytecode(2)
-IMUL = Bytecode(3)
-IEQ = Bytecode(5)
-ILT = Bytecode(4)
-BR = Bytecode(6, 1)
-BRT = Bytecode(7, 1)
-BRF = Bytecode(8, 1)
-ICONST = Bytecode(9, 1)
-LOAD = Bytecode(10, 1)
-GLOAD = Bytecode(11, 1)
-STORE = Bytecode(12, 1)
-GSTORE = Bytecode(13, 1)
-PUTS = Bytecode(14)
-POP = Bytecode(15, 1)
-CALL = Bytecode(16, 1)
-RET = Bytecode(17, 1)
-HALT = Bytecode(18)
+INVALID = Bytecode("INVALID", 0)
+IADD = Bytecode("IADD", 1)
+ISUB = Bytecode("ISUB", 2)
+IMUL = Bytecode("IMUL", 3)
+IEQ = Bytecode("IEQ", 5)
+ILT = Bytecode("IIT", 4)
+BR = Bytecode("BR", 6, 1)
+BRT = Bytecode("BRR", 7, 1)
+BRF = Bytecode("BRF", 8, 1)
+ICONST = Bytecode("ICONST", 9, 1)
+LOAD = Bytecode("LOAD", 10, 1)
+GLOAD = Bytecode("GLOAD", 11, 1)
+STORE = Bytecode("STORE", 12, 1)
+GSTORE = Bytecode("GSTORE", 13, 1)
+PUTS = Bytecode("PUTS", 14)
+POP = Bytecode("POP", 15, 1)
+CALL = Bytecode("CALL", 16, 1)
+RET = Bytecode("RET", 17, 1)
+HALT = Bytecode("HALT", 18)
