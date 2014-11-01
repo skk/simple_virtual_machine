1527a6e295a6bd0c760de03bf6d1cbda1ae6baa4
Steven Knight <steven@knight.cx>
2014-11-01 16:03:45 -0400

    Added project-wide logging config.

--- src/simple-virtual-machine/simplevirtualmachine/__init__.py
+++ src/simple-virtual-machine/simplevirtualmachine/__init__.py
@@ -1,14 +1,40 @@
 '''
 simple-virtual-machine: Main module
-
-Copyright 2014, Steven Knight
-Licensed under MIT.
 '''
 
-from docopt import docopt
+import logging
+import logging.config
+
+LOGGING_CONFIG = {
+    'version': 1,              
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level':'DEBUG',    
+            'class':'logging.StreamHandler',
+        },  
+    },
+    'loggers': {
+        '': {                  
+            'handlers': ['default'],        
+            'level': 'DEBUG',  
+            'propagate': True  
+        },
+    }
+}
+
+logging.config.dictConfig(LOGGING_CONFIG)
+
+
+
 
 def main():
-    
+    from docopt import docopt
     usage = """A Simple Virtual Machine
 
 Usage:
@@ -22,3 +48,6 @@ Options:
 """
     arguments = docopt(usage, version='simple version machien 1.0')
     print arguments
+
+if __name__ == '__main__':
+    main()
