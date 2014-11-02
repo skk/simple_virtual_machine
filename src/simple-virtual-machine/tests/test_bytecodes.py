53884b30b07ae7fff7c164238a4799e4db6e772b
Steven Knight <steven@knight.cx>
2014-11-02 17:21:27 -0500

    Updated tests verify that the opcode.name is correct.

--- src/simple-virtual-machine/tests/test_bytecodes.py
+++ src/simple-virtual-machine/tests/test_bytecodes.py
@@ -4,78 +4,78 @@ from simplevirtualmachine.bytecodes import INVALID, IADD, ISUB, IMUL, \
 
 
 def test_invalid():
-    assert INVALID.opcode == 0 and INVALID.operand_count == 0
+    assert INVALID.opcode == 0 and INVALID.operand_count == 0 and INVALID.name == "INVALID"
 
 
 def test_iadd():
-    assert IADD.opcode == 1 and IADD.operand_count == 0
+    assert IADD.opcode == 1 and IADD.operand_count == 0 and IADD.name == "IADD"
 
 
 def test_isub():
-    assert ISUB.opcode == 2 and ISUB.operand_count == 0
+    assert ISUB.opcode == 2 and ISUB.operand_count == 0 and ISUB.name == "ISUB"
 
 
 def test_imul():
-    assert IMUL.opcode == 3 and IMUL.operand_count == 0
+    assert IMUL.opcode == 3 and IMUL.operand_count == 0 and IMUL.name == "IMUL"
 
 
 def test_ilt():
-    assert ILT.opcode == 4 and ILT.operand_count == 0
+    assert ILT.opcode == 4 and ILT.operand_count == 0 and ILT.name == "ILT"
 
 
 def test_ieq():
-    assert IEQ.opcode == 5 and IEQ.operand_count == 0
+    assert IEQ.opcode == 5 and IEQ.operand_count == 0 and IEQ.name == "IEQ"
 
 
 def test_br():
-    assert BR.opcode == 6 and BR.operand_count == 1
+    assert BR.opcode == 6 and BR.operand_count == 1 and BR.name == "BR"
 
 
 def test_brt():
-    assert BRT.opcode == 7 and BRT.operand_count == 1
+    assert BRT.opcode == 7 and BRT.operand_count == 1 and BRT.name == "BRT"
     
 
 def test_brf():
-    assert BRF.opcode == 8 and BRF.operand_count == 1
+    assert BRF.opcode == 8 and BRF.operand_count == 1 and BRF.name == "BRF"
    
 
 def test_iconst():
-    assert ICONST.opcode == 9 and ICONST.operand_count == 1
+    assert ICONST.opcode == 9 and ICONST.operand_count == 1 and ICONST.name == "ICONST"
 
 
 def test_load():
-    assert LOAD.opcode == 10 and LOAD.operand_count == 1
+    assert LOAD.opcode == 10 and LOAD.operand_count == 1 and LOAD.name == "LOAD"
 
 
 def test_gload():
-    assert GLOAD.opcode == 11 and GLOAD.operand_count == 1
+    assert GLOAD.opcode == 11 and GLOAD.operand_count == 1 and GLOAD.name == "GLOAD"
 
 
 def test_store():
-    assert STORE.opcode == 12 and STORE.operand_count == 1
+    assert STORE.opcode == 12 and STORE.operand_count == 1 and STORE.name == "STORE"
 
 
 def test_gstore():
-    assert GSTORE.opcode == 13 and GSTORE.operand_count == 1
+    assert GSTORE.opcode == 13 and GSTORE.operand_count == 1 and GSTORE.name == "GSTORE"
 
 
 def test_puts():
-    assert PUTS.opcode == 14 and PUTS.operand_count == 0
+    assert PUTS.opcode == 14 and PUTS.operand_count == 0 and PUTS.name == "PUTS"
 
 
 def test_pop():
-    assert POP.opcode == 15 and POP.operand_count == 1
+    assert POP.opcode == 15 and POP.operand_count == 1 and POP.name == "POP"
 
 
 def test_call():
-    assert CALL.opcode == 16 and CALL.operand_count == 2
+    assert CALL.opcode == 16 and CALL.operand_count == 2 and CALL.name == "CALL"
  
 
 def test_ret():
-    assert RET.opcode == 17 and RET.operand_count == 0
+    assert RET.opcode == 17 and RET.operand_count == 0 and RET.name == "RET"
 
 
 def test_halt():
-    assert HALT.opcode == 18 and HALT.operand_count == 0
+    assert HALT.opcode == 18 and HALT.operand_count == 0 and HALT.name == "HALT"
     
     
