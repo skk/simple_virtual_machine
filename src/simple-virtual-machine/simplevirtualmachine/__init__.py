209b422733dcb48ac110678f861030eacb0f92be
Steven Knight <steven@knight.cx>
2014-11-01 20:44:06 -0400

    Switched to configing logging module using logging.basicConfig rather than logging.config.dictConfig.

--- src/simple-virtual-machine/simplevirtualmachine/__init__.py
+++ src/simple-virtual-machine/simplevirtualmachine/__init__.py
@@ -5,33 +5,10 @@ simple-virtual-machine: Main module
 import logging
 import logging.config
 
-LOGGING_CONFIG = {
-    'version': 1,              
-    'disable_existing_loggers': False,
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-        },
-    },
-    'handlers': {
-        'default': {
-            'level':'DEBUG',    
-            'class':'logging.StreamHandler',
-        },  
-    },
-    'loggers': {
-        '': {                  
-            'handlers': ['default'],        
-            'level': 'DEBUG',  
-            'propagate': True  
-        },
-    }
-}
-
-logging.config.dictConfig(LOGGING_CONFIG)
-
-
-
+logging.basicConfig(level=logging.INFO,
+                    datefmt='%y-%m-%d %H:%M:%S',
+                    format='%(asctime)s %(name)-12s %(funcName)s %(filename)s:%(lineno)d %(levelname)-8s %(message)s')
+                    
 
 def main():
     from docopt import docopt
